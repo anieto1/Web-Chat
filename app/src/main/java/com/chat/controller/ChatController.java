580a15ec86fb2948915594808f896be1ef3da370
Alexandro Nieto <anieto1@students.towson.edu>
2025-02-06 14:03:10 -0500

    Added  chat controller and websocket config

---
+++ app/src/main/java/com/chat/controller/ChatController.java
@@ -0,0 +1,23 @@
+package com.chat.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+
+import com.chat.model.ChatMessage;
+@Controller
+public class ChatController {
+
+
+    @MessageMapping("/sendMessage")
+    @SendTo("/topic/messages")
+    public ChatMessage sendMessage(ChatMessage message) {
+        return message;
+    }
+
+    @GetMapping("/chat")
+    public String chat() {
+        return "chat";
+    }
+}
